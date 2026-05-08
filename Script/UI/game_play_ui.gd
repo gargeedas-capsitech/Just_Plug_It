b4ceedb515df2a3c086d98f1678348e806cd0820
sabarnamaity-capsitech <sabarna.maity@capsitech.com>
2026-05-08 15:02:31 +0530

    Gameplay multitouch Funationality

--- Script/UI/game_play_ui.gd
+++ Script/UI/game_play_ui.gd
@@ -1,39 +1,41 @@
 extends CanvasLayer
 
-var _button: TextureButton
-var _rightbutton: TextureButton
-var _leftbutton: TextureButton
-var _upbutton: TextureButton
+var _button: TouchScreenButton
+var _rightbutton: TouchScreenButton
+var _leftbutton: TouchScreenButton
+var _upbutton: TouchScreenButton
 var isPressed =false
 var isRemovePressed =false
 var isRotateleftPressed  = false
 var isRotaterightPressed = false
 var delay = 0;
+
+
 # Called when the node enters the scene tree for the first time.
 @onready var main = get_parent()
 func _ready():
-	_button = get_node("TextureButton")
-	_leftbutton = get_node("TextureButton3")
-	_rightbutton = get_node("TextureButton2")
-	_upbutton = get_node("TextureButton4")
+	_button = get_node("TouchScreenButton")
+	_leftbutton = get_node("TouchScreenButton3")
+	_rightbutton = get_node("TouchScreenButton2")
+	_upbutton = get_node("TouchScreenButton4")
 	
 	if main != null:
 		if main.has_method("rotate_board"):
 			#_rightbutton.pressed.connect(main.rotate_board)
-			_rightbutton.button_down.connect(func(): isRotateleftPressed = true)
-			_rightbutton.button_up.connect(func(): isRotateleftPressed = false)
+			_rightbutton.pressed.connect(func(): isRotateleftPressed = true)
+			_rightbutton.released.connect(func(): isRotateleftPressed = false)
 		if main.has_method("rotate_board_right"):
 			#_leftbutton.pressed.connect(main.rotate_board_right)
-			_leftbutton.button_down.connect(func(): isRotaterightPressed = true)
-			_leftbutton.button_up.connect(func(): isRotaterightPressed = false)	
+			_leftbutton.pressed.connect(func(): isRotaterightPressed = true)
+			_leftbutton.released.connect(func(): isRotaterightPressed = false)	
 		if main.has_method("add_segment"):
 			#_button.pressed.connect(main.add_segment)
-			_button.button_down.connect(func(): isPressed = true)
-			_button.button_up.connect(func(): isPressed = false)
+			_button.pressed.connect(func(): isPressed = true)
+			_button.released.connect(func(): isPressed = false)
 		if main.has_method("remove_segment"):
 			#_upbutton.pressed.connect(main.remove_segment)
-			_upbutton.button_down.connect(func():isRemovePressed=true)
-			_upbutton.button_up.connect(func():isRemovePressed=false)
+			_upbutton.pressed.connect(func():isRemovePressed=true)
+			_upbutton.released.connect(func():isRemovePressed=false)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	delay -= delta
@@ -41,12 +43,17 @@ func _process(delta):
 		return
 	if isPressed and main.has_method("add_segment") and delay<=0:
 		main.add_segment()
-		delay = float(0.1);
+		delay = float(0.1)
+		DebugConsole.debug("pressed add seg")
 	if isRemovePressed and main.has_method("remove_segment") and delay<=0:
+		DebugConsole.debug("pressed remove seg")
 		main.remove_segment()
 		delay = float(0.1);
 	if isRotateleftPressed and main.has_method("rotate_board"):
+		DebugConsole.debug("pressed rotateleft seg")
 		main.rotate_board(delta)
 	if isRotaterightPressed and main.has_method("rotate_board_right"):
+		DebugConsole.debug("pressed rotataeright seg")
 		main.rotate_board_right(delta)
+	
 	
