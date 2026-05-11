e322e631609645d5736b845fdde11d8852b9e134
Protyush-Dey <160174897+Protyush-Dey@users.noreply.github.com>
2026-05-11 16:05:23 +0530

    settingspanel add

---
+++ Script/UI/settings_panelBtn.gd
@@ -0,0 +1,63 @@
+extends CanvasLayer
+
+
+@export var musicBtn : TextureRect
+@export var hapBtn : TextureRect
+@export var soundBtn : TextureRect
+
+@export var musicOn : Texture2D
+@export var musicOff : Texture2D
+@export var hapOn : Texture2D
+@export var hapOff : Texture2D
+@export var soundOn : Texture2D
+@export var soundOff : Texture2D
+
+
+@export var music_on : bool = true
+@export var hap_on : bool = true
+@export var sound_on : bool = true
+
+func _ready():
+	musicBtn.texture = musicOn
+	hapBtn.texture = hapOn
+	soundBtn.texture = soundOn
+func musicbtn():
+	if music_on:
+		musicOffFn()
+	else:
+		musicOnFn()
+	music_on = !music_on
+func hapbtn():
+	if hap_on:
+		hapOffFn()
+	else:
+		hapOnFn()
+	hap_on = !hap_on
+func soundbtn():
+	if sound_on:
+		soundOffFn()
+	else:
+		soundOnFn()
+	sound_on = !sound_on
+func musicOnFn():moveimage(musicBtn,25,musicOn)
+	
+func musicOffFn():moveimage(musicBtn,185,musicOff)
+
+func hapOnFn():moveimage(hapBtn,25,hapOn)
+	
+func hapOffFn():moveimage(hapBtn,185,hapOff)
+
+func soundOnFn():moveimage(soundBtn,25,soundOn)
+	
+func soundOffFn():moveimage(soundBtn,185,soundOff)
+
+func moveimage(btnImage : TextureRect, targetx : float, image : Texture2D):
+	var tween = create_tween()
+	tween.tween_property(
+		btnImage,
+		"position:x",
+		targetx,
+		0.1
+	)
+	await tween.finished
+	btnImage.texture = image
